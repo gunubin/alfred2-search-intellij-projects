326bfd3ab18c37e10c6a03781f76afebedffb5ae
kato.koki <kato@condesire.com>
2014-05-10 10:15:25 +0900

    commit workflow

--- workflow/main.rb
+++ workflow/main.rb
@@ -2,44 +2,42 @@
 # encoding: utf-8
 
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require "bundle/bundler/setup"
+require_relative "bundle/bundler/setup"
 require "alfred"
-
-
+require 'find'
 
 
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
 
-  # add a file feedback
-  fb.add_file_item(File.expand_path "~/Applications/")
+  target = ARGV[0]
+  query = ARGV[1]
 
-  # add an arbitrary feedback
-  fb.add_item({
-    :uid      => ""                     ,
-    :title    => "Just a Test"          ,
-    :subtitle => "feedback item"        ,
-    :arg      => "A test feedback Item" ,
-    :valid    => "yes"                  ,
-  })
-  
-  # add an feedback to test rescue feedback
-  fb.add_item({
-    :uid          => ""                     ,
-    :title        => "Rescue Feedback Test" ,
-    :subtitle     => "rescue feedback item" ,
-    :arg          => ""                     ,
-    :autocomplete => "failed"               ,
-    :valid        => "no"                   ,
-  })
+  item = {
+      :uid      => "",
+      :title    => '',
+      :subtitle => "intellij Project",
+      :arg      => '',
+      :valid    => "yes",
+  }
 
-  if ARGV[0].eql? "failed"
-    alfred.with_rescue_feedback = true
-    raise Alfred::NoBundleIDError, "Wrong Bundle ID Test!"
+  files = Dir.glob(target+"/**/.idea")
+  files.each do |path|
+    if File.directory?(path) && path.include?('.idea')
+      dir = File.dirname(path)
+      pname = File.basename(dir)
+      item[:arg] = dir
+      item[:title] = pname
+      if ARGV.length > 1
+        if pname.include?(query)
+          item[:subtitle] = 'intellij Project query: ' + query
+          fb.add_item(item)
+        end
+      else
+        fb.add_item(item)
+      end
+    end
   end
 
-  puts fb.to_xml(ARGV)
-end
-
-
-
+  puts fb.to_xml
+e
